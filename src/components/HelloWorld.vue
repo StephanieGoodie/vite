b73bba4657014179f470d691a44142ac7fe7ff02
Stephanie Goodie-Obi <goodieobistephanie@gmail.com>
2023-09-17 22:39:36 -0400

    card build

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
+  <!--<div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
@@ -30,7 +30,20 @@ const count = ref(0)
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>-->
+  <div class="card">
+    <img alt="real estate logo" src="./housepic.jpg">
+    <div class="card-content"> 
+      <h2>Two Pod Realty</h2> 
+      <p id="description">Real Estate Company Focused on delivering the best properties in the State College Area</p> 
+      <button class="details-button" id="detailsBtn">Details</button>
+    </div> 
+  </div> 
+  
+  <button id="btn">Duplicator</button>
+  <button id="colorBtn">BGColorChanger</button>
+  <button id="Headingbtn">HeadingChanger</button>
+  <button id="dltbtn">DeleteCard</button>
 </template>
 
 <style scoped>
