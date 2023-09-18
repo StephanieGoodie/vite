cbae9fb6347c74e144a40fb50a37c15e646038db
Stephanie Goodie-Obi <goodieobistephanie@gmail.com>
2023-09-17 22:57:25 -0400

    vue 3 rulesss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,7 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <!--<p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>-->
-</div>
   <div class="card">
     <img alt="real estate logo" src="./housepic.jpg">
     <div class="card-content"> 
@@ -45,6 +16,16 @@ const count = ref(0)
   <button id="Headingbtn">HeadingChanger</button>
   <button id="dltbtn">DeleteCard</button>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
 
 <script>
     document.querySelector('#btn').addEventListener('click', () => {
@@ -135,13 +116,11 @@ h2 {
   padding: 8px 16px;
   border-radius: 4px;
 }
-
 p {
   font-size: 16px;
   margin: 8px 0;
   color: #fff;
 }
-
 a.details-button {
   background-color: #EA5F0A;
   color: #fff;
