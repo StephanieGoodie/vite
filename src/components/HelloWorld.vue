3a5e89be9ae7818af5cd655b0251abe963e25863
Stephanie Goodie-Obi <goodieobistephanie@gmail.com>
2023-09-17 22:53:26 -0400

    commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,10 +10,9 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <!--<div class="card">
+  <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
+    <!--<p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
@@ -31,6 +30,7 @@ const count = ref(0)
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>-->
+</div>
   <div class="card">
     <img alt="real estate logo" src="./housepic.jpg">
     <div class="card-content"> 
@@ -98,6 +98,7 @@ const count = ref(0)
       }
     });
 </script>
+
 <style scoped>
 .read-the-docs {
   color: #888;
